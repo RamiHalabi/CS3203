7c335a2223a6094813217cd697109aa928b269ae
Rami Halabi <ramiihalabi@outlook.com>
2023-02-14 19:15:15 -0600

    Added function for Q5

--- hw3.js
+++ hw3.js
@@ -24,19 +24,36 @@ const computeProduct = (x) => {
   return x.length < 1 ? 0 : x.length == 1 ? x[0] : product;
 };
 
+// Question 5
+const computeReverse = (x) => {
+  const stack = [];
+  const reverse = [];
+
+  // add to stack to begin reversal
+  for (let i = 0; i < x.length; i++) {
+    stack.push(x[i]);
+  }
+
+  // Pop from stack to implement reversal
+  while (stack.length > 0) {
+    reverse.push(stack.pop());
+  }
+
+  return reverse;
+};
 
 // assist for user input
-const readline = require('readline').createInterface({
+const readline = require("readline").createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // Question 3
 const main = () => {
   readline.question("Enter numbers separated by spaces: ", function (input) {
-    const list = input.trim().split(' ').map(Number);
-    console.log(`the sum of [${list}] is `+ computeSum(list));
-    console.log(`the product of [${list}] is `+ computeProduct(list));
+    const list = input.trim().split(" ").map(Number);
+    console.log(`the sum of [${list}] is ` + computeSum(list));
+    console.log(`the product of [${list}] is ` + computeProduct(list));
     readline.close();
   });
 };
