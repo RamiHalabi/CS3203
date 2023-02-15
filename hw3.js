d91568699006ab8a4d520031d96e8b48af651d38
Rami Halabi <ramiihalabi@outlook.com>
2023-02-14 18:34:31 -0600

    Question 2

--- hw3.js
+++ hw3.js
@@ -5,8 +5,7 @@ Homework 3: GIT
 */
 
 // Question 1
-const computeSum = (x) =>{
-
+const computeSum = (x) => {
   // calculate sum
   let sum = 0;
   for (let i = 0; i < x.length; i++) {
@@ -15,5 +14,25 @@ const computeSum = (x) =>{
   return sum;
 };
 
-console.log(computeSum([1,2,3,4,5]));
+console.log(computeSum([1, 2, 3, 4, 5]));
 
+// Question 2
+const computeProduct = (x) => {
+  // base case
+  if (x.length < 1) {
+    // length is 0
+    return 0;
+  } else if (x.length == 1) {
+    // length is 1
+    return x[0];
+  }
+
+  // calculate product
+  let product = 1;
+  for (let i = 0; i < x.length; i++) {
+    product *= x[i];
+  }
+  return product;
+};
+
+console.log(computeProduct([1, 2, 3, 4, 5]));
